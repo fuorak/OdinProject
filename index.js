260cbdc59226450a28bc3b3fe8f4837feea1d868
Matt Goetz <mattgoetz6@gmail.com>
2023-06-21 09:21:46 -0400

    background blue button

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 function alertFunction() {
     alert("This is a test");
-    Console.log("test");
 }
 
 const btn = document.querySelector('#btn');
-btn.addEventListener('click', alertFunction);
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
